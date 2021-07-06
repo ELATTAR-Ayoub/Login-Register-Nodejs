44145a405787600d5c5eafaccf12f1b826f79d19
ELATTAR-Ayoub <82353773+ELATTAR-Ayoub@users.noreply.github.com>
2021-07-06 02:06:30 +0100

    Add files via upload

---
+++ passport-config.js
@@ -0,0 +1,31 @@
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt');
+
+
+function initialize(passport, getUserByName, getUserById) {
+    const authenticateUser = async (username, password, done) => {
+        const user = getUserByName(username)
+        if (user == null) {
+            return done(null, false, { message: 'No user with that name' })
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: 'Password incorrect' })
+            }
+        }
+        catch (e) {
+            return done(e);
+        }
+    }
+    passport.use(new localStrategy({ usernameField: 'username' }, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+
+}
+
+module.exports = initialize;
